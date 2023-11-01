eff442d94e5682d330436eb005474b9a1b73ecbd
CaitlinJadeMay <caitlinjadelingeveldt@gmail.com>
2023-11-01 09:12:32 -0700

    package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,7 @@
-@@ -1,6 +1,10 @@
-# Installs puppet-lint
-#This puppet mainfest installs flask from pip3
-package { 'python3-pip':
-  ensure => 'installed',
-}
+# install puppet from pip3
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
+package { 'flask':
+  ensure   => '2.1.0',
+  name     => 'flask',
   provider => 'pip3',
-exec { 'install flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 freeze | /bin/grep Flask | /bin/grep 2.1.0',
 }
